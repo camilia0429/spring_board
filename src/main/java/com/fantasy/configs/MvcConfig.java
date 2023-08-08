07c71b196b56a2231421aac9c1418e9b0f49868c
camilia0429 <jm_0324@naver.com>
2023-08-09 00:48:49 +0900

    템플릿 레이아웃 프론트 완료

--- src/main/java/com/fantasy/configs/MvcConfig.java
+++ src/main/java/com/fantasy/configs/MvcConfig.java
@@ -1,15 +1,21 @@
 package com.fantasy.configs;
 
 
+import com.fantasy.configs.interceptors.SiteConfigInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
+    // 사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
     @Value("${file.upload.path}")
     private String fileUploadPath;
 
@@ -23,5 +29,11 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
                 .setViewName("main/index");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
     }
 }
